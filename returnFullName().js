70d147da0fc683c3985a7644f1fac8ecd4589907
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-03-21 16:13:35 +0000

    Created a names object for first and last name arrays --> mini database.

--- returnFullName().js
+++ returnFullName().js
@@ -1,23 +1,27 @@
-let firstNames = ["Louise", "Sarah", "Anna", "Steph"];
-let surnames = ["Bailey", "Parker", "Gunn"];
+let names = {
+firstNames: ["Louise", "Sarah", "Anna", "Steph"],
+surnames: ["Bailey", "Parker", "Gunn"],
+}
 let firstName = "";
 let surname = ""; 
-
-function returnFullName(searchName, searchSurname) {
-first: for (let i = 0; i < firstNames.length; ++i) {
-           console.log(firstNames[i]);
-           if (firstNames[i] == searchName) {
-           firstName = firstNames[i]; 
+function returnFullName(searchName) {
+  let nameArray = searchName.split(" ");
+  first: for (let i = 0; i < names.firstNames.length; ++i) {
+           console.log(names.firstNames[i]);
+           if (names.firstNames[i] == nameArray[0]) {
+           firstName = names.firstNames[i]; 
                     } 
            else continue;
-           last: for (let i = 0; i < surnames.length; ++i) { 
-            console.log(surnames[i]);
-            if (surnames[i] == searchSurname) {
-            surname = surnames[i];
+           last: for (let i = 0; i < names.surnames.length; ++i) { 
+            console.log(names.surnames[i]);
+            if (names.surnames[i] == nameArray[1]) {
+            surname = names.surnames[i];
+        //Breaks out of both loops now full name has been found: 
             break first;
             }
            }
 }
 let fullName = firstName + " " + surname;
+return fullName;
 console.log(fullName);
 }
