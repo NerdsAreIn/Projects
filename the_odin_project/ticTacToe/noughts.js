a69ca92506f4408dcd60463921e79de93f18295a
NerdsAreIn <annacantthinkofagoodaddress@hotmail.co.uk>
2021-08-16 17:29:58 +0100

    Got basic gameplay logic working.

--- the_odin_project/ticTacToe/noughts.js
+++ the_odin_project/ticTacToe/noughts.js
@@ -1,5 +1,10 @@
 const gameBoard = (function() {
 	let player;
+	let winner;
+	let noughtsAndCrosses = [];
+	let sign;
+    let signContainer;
+	let squares = Array.from(document.getElementsByClassName("squares"));
 	const takeTurns = function() {
 		if (player == player1) player = player2;
         else player = player1;
@@ -7,31 +12,48 @@ const gameBoard = (function() {
         return player;
 	};
 	const createSign = function() {
+        signContainer = document.createElement("div");
         sign = document.createTextNode(player.symbol);
-        //sign.className = "sign";         
+        signContainer.appendChild(sign);
+        if (sign.nodeValue == "O")  {
+			signContainer.className = "nought";
+        }             
 		console.log({sign});
-        return sign;
-	};
+        return signContainer;
+	};   
 	const checkForWinner = function() {
-
+		if (noughtsAndCrosses[1] == "O" && noughtsAndCrosses[5] == "O" && noughtsAndCrosses[9] == "O" ||		noughtsAndCrosses[2] == "O" && noughtsAndCrosses[5] == "O" && noughtsAndCrosses[8] == "O" || 		    noughtsAndCrosses[7] == "O" && noughtsAndCrosses[5] == "O" && noughtsAndCrosses[3] == "O" || 		    noughtsAndCrosses[4] == "O" && noughtsAndCrosses[5] == "O" && noughtsAndCrosses[6] == "O" || 			noughtsAndCrosses[1] == "O" && noughtsAndCrosses[4] == "O" && noughtsAndCrosses[7] == "O" ||			noughtsAndCrosses[3] == "O" && noughtsAndCrosses[6] == "O" && noughtsAndCrosses[9] == "O" || 		    noughtsAndCrosses[1] == "O" && noughtsAndCrosses[2] == "O" && noughtsAndCrosses[3] == "O" || 		    noughtsAndCrosses[7] == "O" && noughtsAndCrosses[8] == "O" && noughtsAndCrosses[9] == "O"  ) {
+			winner = player1;
+            alert(`The winner is ${winner.name}!`);
+            console.log({winner});
+            return winner;
+		}
+		else if (noughtsAndCrosses[1] == "X" && noughtsAndCrosses[5] == "X" && noughtsAndCrosses[9]  			=="X" || noughtsAndCrosses[2] == "X" && noughtsAndCrosses[5] == "X" && noughtsAndCrosses[8] =="X"
+        || noughtsAndCrosses[7] == "X" && noughtsAndCrosses[5] == "X" && noughtsAndCrosses[3] == "X"
+        || noughtsAndCrosses[4] == "X" && noughtsAndCrosses[5] == "X" && noughtsAndCrosses[6] == "X"||			noughtsAndCrosses[1] == "X" && noughtsAndCrosses[4] == "X" && noughtsAndCrosses[7] == "X" ||			noughtsAndCrosses[3] == "X" && noughtsAndCrosses[6] == "X" && noughtsAndCrosses[9] == "X" || 		    noughtsAndCrosses[1] == "X" && noughtsAndCrosses[2] == "X" && noughtsAndCrosses[3] == "X" || 		    noughtsAndCrosses[7] == "X" && noughtsAndCrosses[8] == "X" && noughtsAndCrosses[9] == "X"   ) {
+			winner = player2;
+            alert(`The winner is ${winner.name}!`);
+            console.log({winner});
+            return winner;
+		}
+        else if (Object.values(noughtsAndCrosses).length == 9 && noughtsAndCrosses.length == 10 && winner == undefined) alert("It's a tie!");	
 	};
-
-	let noughtsAndCrosses = [];
-	let sign;
-	let squares = Array.from(document.getElementsByClassName("squares"));
-	for (let i = 0; i < squares.length; i++) {
+    for (let i = 0; i < squares.length; i++) {
 		squares[i].addEventListener("click", function() {
             takeTurns();
 			createSign();
-            if (!(squares[i].contains(sign))) {
-            squares[i].appendChild(sign);
-			noughtsAndCrosses[squares[i].id] = sign.nodeValue;
-            console.log({noughtsAndCrosses});
+            if (!(squares[i].hasChildNodes())) {
+				squares[i].appendChild(signContainer);
+				noughtsAndCrosses[squares[i].id] = sign.nodeValue;
+            	console.log({noughtsAndCrosses});
             }
-            else return;			
-			//checkForWinner();			
+            else {
+				takeTurns();
+				createSign();
+            }            	
+			checkForWinner();			
 		});
-	}	
+	}   
 })();
 
 const createPlayer = (name, symbol) => {
